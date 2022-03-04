28eccd4a39aac17187d574ec629e1d4a2f85cabc
Matthew Morgan <mattmorgan@khanacademy.org>
2022-03-04 11:25:05 -0500

    Complete first exercise

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -3,19 +3,23 @@
 
 import * as React from 'react'
 
-function Greeting() {
+type Props = {
+  initialName?: string,
+}
+function Greeting({initialName}: Props) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState<string>(initialName)
 
   function handleChange(event: React.SyntheticEvent<HTMLInputElement>) {
     // 🐨 update the name here based on event.currentTarget.value
+    setName(event.currentTarget.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name}/>
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +27,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="world"/>
 }
 
 export default App
